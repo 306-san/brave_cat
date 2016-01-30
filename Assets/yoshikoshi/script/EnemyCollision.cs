924db35272dfdb50dfacc14096b1d34b2eadfba1
popolaty <popolaty@gmail.com>
2016-01-30 19:47:40 +0900

    エネミーの接触

---
+++ Assets/yoshikoshi/script/EnemyCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(GameObject.Find("Player"));
+        }
+    }
+
+}
