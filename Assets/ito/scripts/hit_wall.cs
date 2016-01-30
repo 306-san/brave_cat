5017360eb16b62e63ac6678b9f39cb1e11030427
306san <oh.taku528+git@gmail.com>
2016-01-30 18:15:30 +0900

    敵が動くように

---
+++ Assets/ito/scripts/hit_wall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hit_wall : MonoBehaviour {
+
+// Use this for initialization
+void Start () {
+
+}
+
+// Update is called once per frame
+void Update () {
+
+}
+
+void OnTriggerEnter2D(Collider2D collision)
+{
+         Debug.Log ("hit!!!");
+        if (collision.CompareTag("enemy")) {
+                Destroy(gameObject);
+        }
+}
+
+}
