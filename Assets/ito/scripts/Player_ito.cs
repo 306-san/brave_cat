8e4bfa0b921795068146d9bb051dc67aeca1ef83
306san <oh.taku528+git@gmail.com>
2016-01-31 00:11:45 +0900

    空腹ゲージの実装（バックエンドのみ）

---
+++ Assets/ito/scripts/Player_ito.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_ito : MonoBehaviour
+{
+SpriteRenderer MainSpriteRenderer;
+public Sprite BackSprite;
+public Sprite FrontSprite;
+public Sprite LeftSprite;
+public Sprite RightSprite;
+public float speed = 5;
+public float hungry = 1;
+// Use this for initialization
+void Start()
+{
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+}
+
+// Update is called once per frame
+void Update()
+{
+        if (hungry > 0) {
+                if (Input.GetKey("up"))
+                {
+                        hungry -= 0.0005f;
+                        ChangeStateToBack();
+                }
+                else if (Input.GetKey("down"))
+                {
+                        hungry -= 0.0005f;
+                        ChangeStateToFront();
+                }
+                else if (Input.GetKey("left"))
+                {
+                        hungry -= 0.0005f;
+                        ChangeStateToLeft();
+                }
+                else if (Input.GetKey("right"))
+                {
+                        hungry -= 0.0005f;
+                        ChangeStateToRight();
+                }
+        } else {
+                hungry = 0;
+        }
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x, y).normalized;
+        GetComponent<Rigidbody2D>().velocity = direction * speed * hungry;
+}
+
+
+void ChangeStateToBack()
+{
+        MainSpriteRenderer.sprite = BackSprite;
+}
+
+void ChangeStateToFront()
+{
+        MainSpriteRenderer.sprite = FrontSprite;
+}
+
+void ChangeStateToLeft()
+{
+        MainSpriteRenderer.sprite = LeftSprite;
+}
+
+void ChangeStateToRight()
+{
+        MainSpriteRenderer.sprite = RightSprite;
+}
+void recovery_hungry(){
+        hungry = 1;
+}
+}
---
+++ Assets/ito/scripts/hit_fish.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hit_fish : MonoBehaviour {
+  PlayerControlScript playerControlScript;
+  public GameObject Player;
+	// Use this for initialization
+	void Start () {
+      Player = GameObject.Find("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+  void OnTriggerEnter2D(Collider2D collision)
+  {
+          if (collision.CompareTag("Player")) {
+            Player.SendMessage("recovery_hungry");
+            Destroy(gameObject);
+          }
+  }
+}
